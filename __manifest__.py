5c6b0e72c3a1f040348334289464e6f8e889dab5
Felipe Montero Jiménez <fmj@xalachi.com>
2023-01-20 17:33:19 -0600

    actualizacion

--- __manifest__.py
+++ __manifest__.py
@@ -24,6 +24,7 @@
         'view/planilla.xml',
         'view/boleta.xml',
         'view/transportes.xml',
+        'report/reporte_planilla.xml',
     ],
     'css': [],
     'test': [],
--- model/hr_payslip.py
+++ model/hr_payslip.py
@@ -45,7 +45,10 @@ class hr_payslip_inherit_planilla(models.Model):
     def get_boletas(self):
         viajes = 0
         costo_viajes = 0
-        boletas = self.env['boleta'].search([('date', '>=', self.date_from), ('date', '<=', self.date_to), ('chofer', '=', self.employee_id.id)])
+        boletas = self.env['boleta'].search([
+            ('date', '>=', self.date_from), ('date', '<=', self.date_to),
+            '|', '|', ('chofer', '=', self.employee_id.id), ('ayudante1', '=', self.employee_id.id), ('ayudante2', '=', self.employee_id.id)
+        ])
 
         viajes = len(boletas)
         for boleta in boletas:
@@ -86,3 +89,88 @@ class hr_payslip_inherit_planilla(models.Model):
     total_neto = fields.Float("Neto", compute="get_neto")
     total_viaticos = fields.Float("Viáticos", compute="get_viaticos")
     total_pagar = fields.Float("Total", compute="get_total")
+
+
+class hr_payslip_report(models.Model):
+    _name = "hr_payslip_report"
+
+    def get_company(self):
+        return self.env.company.id
+
+    def obtener_planilla(self):
+        self.env['hr_payslip_report_line'].search([('report', '=', self.id)]).unlink()
+        planillas = self.env['hr.payslip'].search([('date_from', '>=', self.date_from), ('date_to', '<=', self.date_to)])
+        for planilla in planillas:
+            salario = 0
+            total = 0
+            for line in planilla.line_ids:
+                if line.code == "BASIC":
+                    salario = line.total
+                if line.code == "TOTAL":
+                    total = line.total
+            self.env['hr_payslip_report_line'].create({
+                'name': planilla.employee_id.id,
+                'salario': salario,
+                'prestamos': planilla.prestamo,
+                'ahorro': planilla.ahorro,
+                'bonif': planilla.bonific,
+                'depositado': total,
+                'date_del': planilla.date_from,
+                'date_hasta': planilla.date_to,
+                'report': self.id,
+            })
+
+        self.env['hr_payslip_report_gasto'].search([('report', '=', self.id)]).unlink()
+        gastos = self.env['hr.payslip'].search([('date_from', '>=', self.date_from), ('date_to', '<=', self.date_to)])
+        for gasto in gastos:
+            self.env['hr_payslip_report_gasto'].create({
+                'name': gasto.employee_id.id,
+                'peajes': 0,
+                'noches': gasto.costo_noches,
+                'por_viaje': 0,
+                'adelanto': gasto.adelantos,
+                'calzado': gasto.otras_deduc,
+                'depositado': (0 + gasto.costo_noches + 0) - gasto.adelantos - gasto.otras_deduc,
+                'fecha_del': gasto.date_from,
+                'fecha_al': gasto.date_to,
+                'report': self.id,
+            })
+
+    name = fields.Char("Name")
+    company = fields.Many2one("res.company", string="Company", default=get_company)
+    date_from = fields.Date("De")
+    date_to = fields.Date("A")
+    planillas = fields.One2many("hr_payslip_report_line", "report")
+    gastos = fields.One2many("hr_payslip_report_gasto", "report")
+
+
+class hr_payslip_report_line(models.Model):
+    _name = "hr_payslip_report_line"
+
+    name = fields.Many2one("hr.employee", string="Empleado")
+    cedula = fields.Char(related="name.identification_id")
+    oficio = fields.Many2one("hr.job", related="name.job_id")
+    salario = fields.Float("Salario Semanal")
+    prestamos = fields.Float("Prestamos")
+    ahorro = fields.Float("Ahorro")
+    bonif = fields.Float("Bonificaciones o feriados")
+    depositado = fields.Float("Depositado")
+    date_del = fields.Date("Del")
+    date_hasta = fields.Date("Hasta")
+    report = fields.Many2one("hr_payslip_report")
+
+
+class hr_payslip_report_gasto(models.Model):
+    _name = "hr_payslip_report_gasto"
+
+    name = fields.Many2one("hr.employee", string="Empleado")
+    oficio = fields.Many2one("hr.job", related="name.job_id")
+    peajes = fields.Float("Peajes")
+    noches = fields.Integer("Noches")
+    por_viaje = fields.Float("Por viaje")
+    adelanto = fields.Float("Adelanto")
+    calzado = fields.Float("Calzado")
+    depositado = fields.Float("Depositado")
+    fecha_del = fields.Date("Del")
+    fecha_al = fields.Date("Al")
+    report = fields.Many2one("hr_payslip_report")
